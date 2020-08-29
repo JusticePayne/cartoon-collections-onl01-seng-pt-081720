953a2606d8dcdebcfa41206eb4cd43cc63c59e79
Justice Payne <spikepayne@yahoo.com>
2020-08-29 19:59:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,29 +25,3 @@ def find_the_cheese(array)
     i += 1
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
